0542ecaa524bfb13249e0867a30ef707e7dfd3d8
Hernandez-JR <rasamoelyhernandez@gmail.com>
2026-05-19 15:36:16 +0300

    clientcreneaux

--- app/Controllers/CreneauxController.php
+++ app/Controllers/CreneauxController.php
@@ -8,7 +8,10 @@ class CreneauxController extends Controller
 {
  public function creneaux()
     {
-        return view('creneaux');        
+        $db = \config\Database::connect();
+        $query = $db->query("SELECT * FROM creneaux JOIN ressources ON creneaux.ressource_id = ressources.id");
+        $data['creneaux'] = $query->getResultArray();
+        return view('creneaux', $data);        
     }
 
     public function adminCreneaux()
--- database/insert.sql
+++ database/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO ressources (nom, type, capacite, description) VALUES
-('Salle de réunion A', 'Salle de réunion', 10, 'Salle de réunion équipée d un projecteur et d une connexion Wi-Fi.'),
-('Salle de réunion B', 'Salle de réunion', 20, 'Grande salle de réunion avec une table ovale et des chaises confortables.'),
-('Bureau privé 1', 'Bureau privé', 2, 'Bureau privé pour deux personnes avec une vue sur la ville.'),
-('Bureau privé 2', 'Bureau privé', 4, 'Bureau privé pour quatre personnes avec un espace de travail collaboratif.'),
-('Espace de coworking', 'Espace de coworking', 50, 'Espace de coworking ouvert avec des bureaux partagés et des zones de détente.');
+('Yoga Detente', 'COURS', 10, 'Salle Zen · 2e étage'),
+('CrossFit Intensif', 'COURS', 20, 'Grande salle'),
+('Salle de Musculation', 'SALLE', 2, 'Salle de musculation avec équipements modernes.'),
+('Salle de Réunion', 'SALLE', 4, 'Bureau privé avec un espace de travail collaboratif.'),
+('Terrain de Tennis', 'TERRAIN', 50, 'Court A avec surface en dur');
 
 INSERT INTO creneaux (ressource_id, date_debut, date_fin, place_dispo, actif) VALUES
 (1, '2024-07-01 09:00:00', '2024-07-01 10:00:00', 10, 1),
