153c47e1adb6720ce3f8d280cf3a78321b6ca63c
Hernandez-JR <rasamoelyhernandez@gmail.com>
2026-05-19 14:34:08 +0300

    affListeCreneau

--- app/Controllers/CreneauxController.php
+++ app/Controllers/CreneauxController.php
@@ -13,6 +13,9 @@ class CreneauxController extends Controller
 
     public function adminCreneaux()
     {
-        return view('admin/creneaux');        
+        $db = \config\Database::connect();
+        $query = $db->query("SELECT * FROM creneaux JOIN ressources ON creneaux.ressource_id = ressources.id");
+        $data['creneaux'] = $query->getResultArray(); 
+        return view('/admin/creneaux', $data);
     }
 }
