185f6a5ee15905950d32a690444ab5c46f6ba144
Honore160306 <randriamihajahonore1@gmail.com>
2026-05-19 13:32:28 +0300

    Insert

--- database/insert.sql
+++ database/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO ressources (nom, type, capacite, description) VALUES
-('Salle de réunion A', 'Salle de réunion', 10, 'Salle de réunion équipée d\'un projecteur et d\'une connexion Wi-Fi.'),
+('Salle de réunion A', 'Salle de réunion', 10, 'Salle de réunion équipée d un projecteur et d une connexion Wi-Fi.'),
 ('Salle de réunion B', 'Salle de réunion', 20, 'Grande salle de réunion avec une table ovale et des chaises confortables.'),
 ('Bureau privé 1', 'Bureau privé', 2, 'Bureau privé pour deux personnes avec une vue sur la ville.'),
 ('Bureau privé 2', 'Bureau privé', 4, 'Bureau privé pour quatre personnes avec un espace de travail collaboratif.'),
