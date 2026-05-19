9bca144f67f6bcb27ad05403e3e1b36f49a6284c
Hernandez-JR <rasamoelyhernandez@gmail.com>
2026-05-19 16:31:43 +0300

    insert

--- database/insert.sql
+++ database/insert.sql
@@ -1,3 +1,8 @@
+INSERT INTO users (nom, email, password, role) VALUES
+('Honore', 'honore@gmail.com', 'honore', 'client'),
+('Joshua', 'joshua@gmail.com', 'joshua', 'client'),
+('Marino', 'marino@gmail.com', 'marino', 'client');
+
 INSERT INTO ressources (nom, type, capacite, description) VALUES
 ('Yoga Detente', 'COURS', 10, 'Salle Zen · 2e étage'),
 ('CrossFit Intensif', 'COURS', 20, 'Grande salle'),
@@ -12,3 +17,8 @@ INSERT INTO creneaux (ressource_id, date_debut, date_fin, place_dispo, actif) VA
 (3, '2024-07-01 09:00:00', '2024-07-01 17:00:00', 2, 1),
 (4, '2024-07-01 09:00:00', '2024-07-01 17:00:00', 4, 1),
 (5, '2024-07-01 09:00:00', '2024-07-01 17:00:00', 50, 1);
+
+INSERT INTO reservations (user_id, creneau_id, statut, created_at) VALUES
+(1, 1, 0, '2024-06-20 12:00:00'),
+(2, 1, 0, '2024-06-20 12:05:00'),
+(3, 2, 0, '2024-06-20 12:10:00');
