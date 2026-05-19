c24b615238b6afd46ea145fba7d489483d4213b2
Honore160306 <randriamihajahonore1@gmail.com>
2026-05-19 15:47:51 +0300

    merge

--- app/Controllers/CreneauxController.php
+++ app/Controllers/CreneauxController.php
@@ -17,6 +17,33 @@ class CreneauxController extends Controller
 
     public function adminCreneaux()
     {
-        return view('admin/creneaux');        
+<<<<<<< HEAD
+        $db = \Config\Database::connect();
+        $query = $db->query('SELECT * FROM ressources');
+        $data['ressources'] = $query->getResultArray();
+        return view('admin/creneaux', $data);        
+    }
+
+    public function ajoutCreneaux()
+    {
+        $creneauxModel = new CreneauxModel();
+        $creneauxModel->insert([
+            'ressource_id' => $this->request->getPost('ressource_id'),
+            'date_debut' => $this->request->getPost('date_debut'),
+            'date_fin' => $this->request->getPost('date_fin'),
+            'place_dispo' => $this->request->getPost('place_dispo'),
+            'actif' => 1
+        ]);
+        
+        $db = \Config\Database::connect();
+        $query = $db->query('SELECT * FROM ressources');
+        $data['ressources'] = $query->getResultArray();
+        return view('admin/creneaux', $data);    
+=======
+        $db = \config\Database::connect();
+        $query = $db->query("SELECT * FROM creneaux JOIN ressources ON creneaux.ressource_id = ressources.id");
+        $data['creneaux'] = $query->getResultArray(); 
+        return view('/admin/creneaux', $data);
+>>>>>>> 0542ecaa524bfb13249e0867a30ef707e7dfd3d8
     }
 }
